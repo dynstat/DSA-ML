a4818653772c8a6fcd76442990c0679094c0e61c
VIVEK SINGH <vs15vivek@gmail.com>
2025-12-25 10:10:34 +0530

    feat: Add solution for LeetCode problem 11, "Container With Most Water".

---
+++ 25Dec25/11.container-with-most-water.py
@@ -0,0 +1,78 @@
+#
+# @lc app=leetcode id=11 lang=python3
+#
+# [11] Container With Most Water
+#
+# https://leetcode.com/problems/container-with-most-water/description/
+#
+# algorithms
+# Medium (59.15%)
+# Likes:    33233
+# Dislikes: 2139
+# Total Accepted:    4.9M
+# Total Submissions: 8.2M
+# Testcase Example:  '[1,8,6,2,5,4,8,3,7]'
+#
+# You are given an integer array height of length n. There are n vertical lines
+# drawn such that the two endpoints of the i^th line are (i, 0) and (i,
+# height[i]).
+# 
+# Find two lines that together with the x-axis form a container, such that the
+# container contains the most water.
+# 
+# Return the maximum amount of water a container can store.
+# 
+# Notice that you may not slant the container.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: height = [1,8,6,2,5,4,8,3,7]
+# Output: 49
+# Explanation: The above vertical lines are represented by array
+# [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the
+# container can contain is 49.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: height = [1,1]
+# Output: 1
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# n == height.length
+# 2 <= n <= 10^5
+# 0 <= height[i] <= 10^4
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        # create two pointers left and right
+        # shift the left pointer if it is smaller than right, else 
+        # shift the right pointer.
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+
+        while left < right: # TODO
+            area = 0
+            if height[left] < height[right]:
+                area = height[left] * (right - left)
+                left += 1
+            else:
+                area = height[right] * (right - left)
+                right -= 1
+
+            max_area = max(max_area, area)
+        return max_area
+# @lc code=end
+
